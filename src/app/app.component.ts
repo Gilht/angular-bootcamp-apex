05ebb38c58e9f5d47884fa30bde33a1dee5ddab2
Gilht <ghdzt14@hotmail.com>
2024-04-16 18:30:01 -0600

    Exercise 1, related ng style and ng class was finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Exercise1Component } from './components/exercise-1/exercise-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Exercise1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
